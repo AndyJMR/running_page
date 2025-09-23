73770a17b2e369b8f43937a782383e2875deff7f
AndyJMR <adamwin2010@gmail.com>
2025-09-23 15:09:26 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,7 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Keep Running',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://running-page-pi-fawn.vercel.app/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'run, Forrest, run...',
   navLinks: [
